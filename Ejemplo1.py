eb04b46cd95bb8ce87ec2e12c88fc0638841c683
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2020-02-05 13:08:19 +0100

    "Ejemplo doc 1"

---
+++ Ejemplo1.py
@@ -0,0 +1,34 @@
+
+
+
+def faiAlgo(numero, letra, cadea):
+    """Función que imprime os parametros e devolta unha cadea.
+
+    La función faiAlgo imprime os parametros de entrada e devolta a cadea de multiplicar o numero pola letra
+    pasada por parametro
+
+    :param numero
+    :param letra
+    :param cadea
+    :return numero * letra
+
+    """
+    print(numero)
+    print(letra)
+    print(cadea)
+
+    return numero * letra
+
+
+def faiOutraCousa (tarefa):
+    """Funcion que imprime a tarefa.
+
+    Funcion que imprime a tarefa por consola.
+
+    :param tarefa
+    :return None
+
+    """
+
+
+    print(tarefa)
